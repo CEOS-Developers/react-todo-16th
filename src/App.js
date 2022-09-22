0b7979596c36d216e218f739d3e68f3002c1a803
Yoo SeonHo <tjsgh503@naver.com>
2022-09-22 17:22:14 +0900

    style : Global Style and Box design

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    background-color: powderblue;
+    font-family: 'SEBANG_Gothic_Bold';
+  }
+`;
+
+const Box = styled.div`
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  align-items: stretch;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  width: 500px;
+  height: 700px;
+  border: 10px black;
+  border-radius: 7%;
+  background-color: white;
+  box-shadow: 1px 1px 3px 1px #dadce0;
+  line-height: 1;
+  margin: auto;
+`;
+
 function App() {
   return (
-    <div>
-      <h1>16기 프론트 화이팅~</h1>
-    </div>
+    <>
+      <GlobalStyle />
+      <Box></Box>
+    </>
   );
 }
 
